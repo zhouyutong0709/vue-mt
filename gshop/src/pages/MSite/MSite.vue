494f174456fbe1fbd748db6ed25de619a03cb511
yutong <1432544296@qq.com>
2019-04-19 14:45:02 +0800

    HeaderTop组件，运用slot，抽取header部分

--- gshop/src/pages/MSite/MSite.vue
+++ gshop/src/pages/MSite/MSite.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="msite">
     <!--首页头部-->
-    <header class="header">
+    <!-- <header class="header">
       <span class="header_search">
         <i class="iconfont icon-sousuo"></i>
       </span>
@@ -11,7 +11,18 @@
       <span class="header_login">
         <span class="header_login_text">登录|注册</span>
       </span>
-    </header>
+    </header>-->
+
+    <!--首页头部-->
+    <HeaderTop title="昌平区北七家宏福科技园(337省道北)">
+      <span class="header_search" slot="left">
+        <i class="iconfont icon-sousuo"></i>
+      </span>
+      <span class="header_login" slot="right">
+        <span class="header_login_text">登录|注册</span>
+      </span>
+    </HeaderTop>
+
     <!--首页导航-->
     <nav class="msite_nav">
       <div class="swiper-container">
@@ -296,7 +307,12 @@
 </template>
 
 <script>
-export default {};
+import HeaderTop from "../../components/HeaderTop/HeaderTop.vue";
+export default {
+  components: {
+    HeaderTop
+  }
+};
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
@@ -304,59 +320,6 @@ export default {};
 
 .msite { // 首页
   width: 100%;
-
-  .header {
-    background-color: #02a774;
-    position: fixed;
-    z-index: 100;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 45px;
-
-    .header_search {
-      position: absolute;
-      left: 15px;
-      top: 50%;
-      transform: translateY(-50%);
-      width: 10%;
-      height: 50%;
-
-      .icon-sousuo {
-        font-size: 25px;
-        color: #fff;
-      }
-    }
-
-    .header_title {
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      width: 50%;
-      color: #fff;
-      text-align: center;
-
-      .header_title_text {
-        font-size: 20px;
-        color: #fff;
-        display: block;
-      }
-    }
-
-    .header_login {
-      font-size: 14px;
-      color: #fff;
-      position: absolute;
-      right: 15px;
-      top: 50%;
-      transform: translateY(-50%);
-
-      .header_login_text {
-        color: #fff;
-      }
-    }
-  }
 
   .msite_nav {
     bottom-border-1px(#e4e4e4);
